fa6893a1a74c1bf9ca087f661e78d0d8ca121927
Alex Rosa <alexr@gec.inatel.br>
2023-11-28 15:02:46 -0300

    first commit - project generated by spring initializr

---
+++ src/main/kotlin/com/example/UdemySpringKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UdemySpringKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<UdemySpringKotlinApplication>(*args)
+}
