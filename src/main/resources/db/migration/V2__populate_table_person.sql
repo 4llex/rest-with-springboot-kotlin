a1fe7d586c6c74ff148ef9dc0f3bf63407cba0a9
Alex Rosa <alexr@gec.inatel.br>
2023-12-04 22:29:00 -0300

    class 92 - Migration com flyway e exportar dados do db

---
+++ src/main/resources/db/migration/V2__populate_table_person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` VALUES
+(2,'Cambui-MG ','Laura Haley Santos','Female','Santos'),
+(3,'Cambui-MG ','Alex','Male','Rosa'),
+(6,'Cambui-MG ','Eric','Male','Clapton'),
+(7,'Cambui-MG ','jimmi','Male','hendrix'),
+(9,'Japao-MG 0','natan','Male 0','east'),
+(10,'USA-MG ','eric jonson','Male','santos'),
+(11,'ENG-MG ','maria jonson','Male','oliveira');
